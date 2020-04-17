e0a986c3465496f4ade0f6ce21dbbe93e43f83ea
raquelsr <raquelserranor@hotmail.com>
2020-04-17 21:02:22 +0200

    Fix CORS problems in server

--- server-side/server.js
+++ server-side/server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 const data = require('./data/phones.json');
+var cors = require('cors');
+
+app.use(cors());
 
 app.listen(3001, () => {
  console.log('Server running on port 3001');
