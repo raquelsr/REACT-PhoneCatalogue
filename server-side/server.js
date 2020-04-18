e15f6b4a1c48f31c77e124d5ae15c4d1256950ea
raquelsr <raquelserranor@hotmail.com>
2020-04-19 00:48:47 +0200

    Server-side: Add get by id

--- server-side/server.js
+++ server-side/server.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
-const data = require('./data/phones.json');
 var cors = require('cors');
+
+const data = require('./data/phones.json');
 
 app.use(cors());
 
@@ -9,6 +10,11 @@ app.listen(3001, () => {
  console.log('Server running on port 3001');
 });
 
-app.get('/phones', (req, res, next) => {
-  res.json(data);
+app.get('/phones', (req, res) => {
+  res.status(200).json(data);
+});
+
+app.get('/phones/:id', (req,res) => {
+  const phone = data.find(phone => phone.id == req.params.id);
+  phone ? res.status(200).json(phone) : res.status(404).end();
 });
