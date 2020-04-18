8993c5f6b1052d8619c033d530b64eb56d8c17f7
raquelsr <raquelserranor@hotmail.com>
2020-04-19 00:30:55 +0200

    Style phone details

--- client-side/src/components/phone.js
+++ client-side/src/components/phone.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import '../styles/phone.css';
 
 const Phone = () => {
   //let { id }  = useParams(); // TODO: Si introduce la url directamente no tiene state
+  //const phone = useSelector(state => state.phoneSelected);
 
-  const phone = useSelector(state => state.phoneSelected);
-  console.log(phone);
+  const phones = useSelector(state => state.phones);
+  const phone = phones[0];
   return (
     <React.Fragment>
-      <p>{phone.name}</p>
-      <p>{phone.description}</p>
+      <h3>{phone.name}</h3>
+      <div className="details">
+        <div className="image">
+          <img src={require(`../../images/${phone.imageFileName}`)} alt={phone.name} />
+        </div>
+        <div className="description">
+          <p>Manufacturer: {phone.manufacturer}</p>
+          <p>Description: {phone.description}</p>
+          <p>Color: {phone.color} </p>
+          <p>Price: {phone.price}</p>
+          <p>Screen: {phone.screen}</p>
+          <p>Processor: {phone.processor}</p>
+          <p>RAM: {phone.ram}</p>
+        </div>
+      </div>
     </React.Fragment>
   );
 }
