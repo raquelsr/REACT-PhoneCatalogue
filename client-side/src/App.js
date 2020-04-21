5f1f9ea052bb66c2778145807c874bffea4064db
raquelsr <raquelserranor@hotmail.com>
2020-04-21 17:32:58 +0200

    Add footer

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import Routes from './routes/router';
+import Footer from './components/footer';
+import './styles/app.css';
 
 function App() {
   return (
-    <Routes />
+    <React.Fragment>
+      <Routes />
+      <Footer />
+    </React.Fragment>
   );
 }
 
